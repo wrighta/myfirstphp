5ee1ef06e5f071dfb620ef3a7834dfa082d82cd6
Anne Wright <anne.wright@iadt.ie>
2021-09-27 15:15:15 +0100

    I made changes to the text in both files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello There <br>";
+echo "Hello There Again <br>";
+?>
+ 
+    
+    
+<a href="myFirstPHP.php">Click here to go to another page</a>
+
+</body>
+</html>
---
+++ myFirstPHP.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$txt1 = "HELLO PHP";
+$txt2 = "IADT.IE";
+$x = 100;
+$y = 200;
+
+// Line 12 is not the problem. It looks fine, so check above... 
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+?>
+
+
+    
+<!-- now put a link here to get back to the first page -->
+<?php
+    echo "Now can you add another link to this page that links back to the first page";
+?>
+    
+</body>
+</html>
