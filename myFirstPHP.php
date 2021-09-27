21d9b9a0c382004ebc8c429a14022843e7559861
Anne Wright <anne.wright@iadt.ie>
2021-09-27 15:59:25 +0100

    Changed Text again

--- myFirstPHP.php
+++ myFirstPHP.php
@@ -1,15 +1,15 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>My XXXX page</h1>
 
 <?php
 $txt1 = "HELLO PHP";
-$txt2 = "IADT.IE";
+$txt2 = "I have changed myFirstPHP too";
 $x = 100;
 $y = 200;
 
-// Line 12 is not the problem. It looks fine, so check above... 
+// Line 12 is nogitt the problem. It looks fine, so check above... 
 echo "<h2>" . $txt1 . "</h2>";
 echo "Study PHP at " . $txt2 . "<br>";
 echo $x + $y;
